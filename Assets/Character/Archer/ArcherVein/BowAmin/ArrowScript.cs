6eb24ab6ec0150e397792ab7c18b079f578da48b
NguyenDuyTan <duytannguyen750@gmail.com>
2026-02-24 21:00:29 +0700

    Add Bow For Archer

---
+++ Assets/Character/Archer/ArcherVein/BowAmin/ArrowScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    public ParticleSystem hitEffect; // Hiệu ứng hạt khi va chạm
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Phát hiệu ứng hạt tại vị trí va chạm
+        if (hitEffect != null)
+        {
+            ParticleSystem effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            effect.Play();
+            Destroy(effect.gameObject, 2f); // Huỷ hiệu ứng sau 2 giây
+        }
+
+        // Huỷ mũi tên sau khi va chạm
+        Destroy(gameObject);
+    }
+}
